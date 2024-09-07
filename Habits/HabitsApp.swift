28a52cd4f97b5a956446b9b5d89e63861a8cf1b3
Asif Kabani <asifkabani@gmail.com>
2024-09-06 22:47:36 -0500

    Initial Commit

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Asif Kabani on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
